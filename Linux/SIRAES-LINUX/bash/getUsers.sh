58bce94bc27b79f43a445470ec77e8de988ca905
seanshx <shon160995@gmail.com>
2023-08-06 15:18:33 +0300

    Added bash scripts

---
+++ Linux/SIRAES-LINUX/bash/getUsers.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+OUTPUT_FILE="user_info.csv"
+
+# CSV headers
+echo "Username,UserID,GroupID,HomeDirectory,Shell,LastLogin,PasswordStatus,Groups,SSHAuthorizedKeys,LastPasswordChange,PasswordExpiration" > $OUTPUT_FILE
+
+# Loop through each user in /etc/passwd
+while IFS: read -r user pass uid gid comment home shell; do
+    # Ignore system users (optional)
+    if [[ $uid -lt 1000 && "$user" != "root" ]]; then
+        continue
+    fi
+
+    # Last login
+    last_login=$(last -n 1 "$user" | head -n 1 | awk '{if (NF==10) print $4" "$5" "$6" "$7" "$8; else print "Never logged in"}')
+
+    # Password status (e.g., if the password is locked)
+    password_status=$(passwd -S "$user" 2>/dev/null | awk '{print $2}')
+
+    # Groups the user belongs to
+    groups_list=$(groups "$user" | cut -d: -f2 | sed 's/^ //')
+
+    # SSH authorized keys (if present)
+    ssh_keys="None"
+    [[ -f "$home/.ssh/authorized_keys" ]] && ssh_keys=$(cat "$home/.ssh/authorized_keys" | tr '\n' ';')
+
+    # Last password change and password expiration
+    last_pass_change=$(chage -l "$user" | grep 'Last password change' | cut -d: -f2)
+    pass_expiry=$(chage -l "$user" | grep 'Password expires' | cut -d: -f2)
+
+    # Append to the CSV
+    echo "$user,$uid,$gid,$home,$shell,\"$last_login\",$password_status,\"$groups_list\",\"$ssh_keys\",\"$last_pass_change\",\"$pass_expiry\"" >> $OUTPUT_FILE
+done < /etc/passwd
