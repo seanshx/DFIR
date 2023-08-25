e6b949c639d4f4f3d64576245bd4d205c6612705
seanshx <shon160995@gmail.com>
2023-08-25 19:27:23 +0300

    cr updated

--- xp_cr.cpp
+++ xp_cr.cpp
@@ -1,118 +1,79 @@
-#include <windows.h>
 #include <iostream>
 #include <vector>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
 
 const int THREAD_COUNT = 6;
 const int MAX_CONCURRENT_THREADS = 3;
 
-// Semaphore handle
-HANDLE hSemaphore;
+std::mutex mtx;                     // Mutex for controlling access to std::cout
+std::mutex semaphore_mtx;          // Mutex for the semaphore logic
+std::condition_variable cv;        // Condition variable for the semaphore logic
+int available_threads = MAX_CONCURRENT_THREADS;
 
-// Array of event handles to determine thread completion
-HANDLE hEvents[THREAD_COUNT];
-
-CRITICAL_SECTION cs; // for synchronizing the std::cout calls
-
-// Six separate functions
 void Function1() {
     std::cout << "Function 1 running." << std::endl;
-    Sleep(1000);
+    std::this_thread::sleep_for(std::chrono::seconds(1));
 }
 
 void Function2() {
     std::cout << "Function 2 running." << std::endl;
-    Sleep(1000);
+    std::this_thread::sleep_for(std::chrono::seconds(1));
 }
 
 void Function3() {
     std::cout << "Function 3 running." << std::endl;
-    Sleep(1000);
+    std::this_thread::sleep_for(std::chrono::seconds(1));
 }
 
 void Function4() {
     std::cout << "Function 4 running." << std::endl;
-    Sleep(1000);
+    std::this_thread::sleep_for(std::chrono::seconds(1));
 }
 
 void Function5() {
     std::cout << "Function 5 running." << std::endl;
-    Sleep(1000);
+    std::this_thread::sleep_for(std::chrono::seconds(1));
 }
 
 void Function6() {
     std::cout << "Function 6 running." << std::endl;
-    Sleep(1000);
+    std::this_thread::sleep_for(std::chrono::seconds(1));
 }
 
-// The general thread function
-DWORD WINAPI ThreadFunction(LPVOID lpParam) {
-    // Wait on the semaphore
-    WaitForSingleObject(hSemaphore, INFINITE);
+void ThreadFunction(void (*func)()) {
+    {
+        std::unique_lock<std::mutex> lock(semaphore_mtx);
+        cv.wait(lock, [] { return available_threads > 0; });
+        --available_threads;
+    }
 
-    // Call the appropriate function
-    void (*func)() = (void(*)())lpParam;
-    
-    // Synchronized output
-    EnterCriticalSection(&cs);
-    func();
-    LeaveCriticalSection(&cs);
+    {
+        std::lock_guard<std::mutex> lock(mtx);
+        func();
+    }
 
-    // Release the semaphore
-    ReleaseSemaphore(hSemaphore, 1, NULL);
-
-    // Signal that this thread is done
-    int index = func == Function1 ? 0 : func == Function2 ? 1 : func == Function3 ? 2 : func == Function4 ? 3 : func == Function5 ? 4 : 5;
-    SetEvent(hEvents[index]);
-
-    return 0;
+    {
+        std::lock_guard<std::mutex> lock(semaphore_mtx);
+        ++available_threads;
+        cv.notify_one();
+    }
 }
 
 int main() {
-    InitializeCriticalSection(&cs);
-
-    hSemaphore = CreateSemaphore(NULL, MAX_CONCURRENT_THREADS, MAX_CONCURRENT_THREADS, NULL);
-    if (hSemaphore == NULL) {
-        std::cerr << "Failed to create semaphore. Error code: " << GetLastError() << std::endl;
-        return 1;
-    }
-
-    // Initialize events
-    for (int i = 0; i < THREAD_COUNT; i++) {
-        hEvents[i] = CreateEvent(NULL, TRUE, FALSE, NULL);
-        if (hEvents[i] == NULL) {
-            std::cerr << "Failed to create event. Error code: " << GetLastError() << std::endl;
-            CloseHandle(hSemaphore);
-            for (int j = 0; j < i; j++) {
-                CloseHandle(hEvents[j]);
-            }
-            return 1;
-        }
-    }
-
     // A vector of function pointers
     std::vector<void(*)()> functions = {Function1, Function2, Function3, Function4, Function5, Function6};
 
-    // Queue threads
+    std::vector<std::thread> threads;
+
     for (int i = 0; i < THREAD_COUNT; i++) {
-        if (!QueueUserWorkItem(ThreadFunction, functions[i], 0)) {
-            std::cerr << "Failed to queue thread. Error code: " << GetLastError() << std::endl;
-            CloseHandle(hSemaphore);
-            for (int j = 0; j < THREAD_COUNT; j++) {
-                CloseHandle(hEvents[j]);
-            }
-            return 1;
-        }
+        threads.emplace_back(ThreadFunction, functions[i]);
     }
 
-    // Wait for all threads to complete
-    WaitForMultipleObjects(THREAD_COUNT, hEvents, TRUE, INFINITE);
-
-    // Cleanup
-    CloseHandle(hSemaphore);
-    for (int i = 0; i < THREAD_COUNT; i++) {
-        CloseHandle(hEvents[i]);
+    for (auto& thread : threads) {
+        thread.join();
     }
-    DeleteCriticalSection(&cs);
 
     std::cout << "All threads completed." << std::endl;
 
