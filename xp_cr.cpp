6be9d012806cfda03f4a188676dfc097d7d8c18a
seanshx <shon160995@gmail.com>
2023-08-25 19:13:09 +0300

    update xp cr

--- xp_cr.cpp
+++ xp_cr.cpp
@@ -1,16 +1,22 @@
 #include <windows.h>
 #include <iostream>
+#include <vector>
 
 const int THREAD_COUNT = 6;
 const int MAX_CONCURRENT_THREADS = 3;
 
-// The semaphore handle
+// Semaphore handle
 HANDLE hSemaphore;
 
-// Six separate functions for the threads
+// Array of event handles to determine thread completion
+HANDLE hEvents[THREAD_COUNT];
+
+CRITICAL_SECTION cs; // for synchronizing the std::cout calls
+
+// Six separate functions
 void Function1() {
     std::cout << "Function 1 running." << std::endl;
-    Sleep(1000); // Simulate some work by sleeping
+    Sleep(1000);
 }
 
 void Function2() {
@@ -38,47 +44,75 @@ void Function6() {
     Sleep(1000);
 }
 
-// The general thread function to be queued
+// The general thread function
 DWORD WINAPI ThreadFunction(LPVOID lpParam) {
     // Wait on the semaphore
     WaitForSingleObject(hSemaphore, INFINITE);
 
-    // Call the appropriate function based on the passed parameter
+    // Call the appropriate function
     void (*func)() = (void(*)())lpParam;
+    
+    // Synchronized output
+    EnterCriticalSection(&cs);
     func();
+    LeaveCriticalSection(&cs);
 
     // Release the semaphore
     ReleaseSemaphore(hSemaphore, 1, NULL);
+
+    // Signal that this thread is done
+    int index = func == Function1 ? 0 : func == Function2 ? 1 : func == Function3 ? 2 : func == Function4 ? 3 : func == Function5 ? 4 : 5;
+    SetEvent(hEvents[index]);
 
     return 0;
 }
 
 int main() {
+    InitializeCriticalSection(&cs);
+
     hSemaphore = CreateSemaphore(NULL, MAX_CONCURRENT_THREADS, MAX_CONCURRENT_THREADS, NULL);
     if (hSemaphore == NULL) {
         std::cerr << "Failed to create semaphore. Error code: " << GetLastError() << std::endl;
         return 1;
     }
 
-    // An array of function pointers to the six functions
-    void (*functions[THREAD_COUNT])() = {Function1, Function2, Function3, Function4, Function5, Function6};
-
-    // Queue threads using QueueUserWorkItem
+    // Initialize events
     for (int i = 0; i < THREAD_COUNT; i++) {
-        if (!QueueUserWorkItem(ThreadFunction, functions[i], 0)) {
-            std::cerr << "Failed to queue thread. Error code: " << GetLastError() << std::endl;
+        hEvents[i] = CreateEvent(NULL, TRUE, FALSE, NULL);
+        if (hEvents[i] == NULL) {
+            std::cerr << "Failed to create event. Error code: " << GetLastError() << std::endl;
             CloseHandle(hSemaphore);
+            for (int j = 0; j < i; j++) {
+                CloseHandle(hEvents[j]);
+            }
             return 1;
         }
     }
 
-    // Waiting for all threads to complete their execution
-    for (int i = 0; i < MAX_CONCURRENT_THREADS; i++) {
-        WaitForSingleObject(hSemaphore, INFINITE);
+    // A vector of function pointers
+    std::vector<void(*)()> functions = {Function1, Function2, Function3, Function4, Function5, Function6};
+
+    // Queue threads
+    for (int i = 0; i < THREAD_COUNT; i++) {
+        if (!QueueUserWorkItem(ThreadFunction, functions[i], 0)) {
+            std::cerr << "Failed to queue thread. Error code: " << GetLastError() << std::endl;
+            CloseHandle(hSemaphore);
+            for (int j = 0; j < THREAD_COUNT; j++) {
+                CloseHandle(hEvents[j]);
+            }
+            return 1;
+        }
     }
-    
-    // At this point, all threads are finished, so it's safe to close the semaphore handle
+
+    // Wait for all threads to complete
+    WaitForMultipleObjects(THREAD_COUNT, hEvents, TRUE, INFINITE);
+
+    // Cleanup
     CloseHandle(hSemaphore);
+    for (int i = 0; i < THREAD_COUNT; i++) {
+        CloseHandle(hEvents[i]);
+    }
+    DeleteCriticalSection(&cs);
 
     std::cout << "All threads completed." << std::endl;
 
